26838103f3a65ccbf9465f0faa85f9724b4f964d
ahtesham@007 <zsyedahtesham@yahoo.com>
2024-12-03 20:06:49 +0530

    added code for negative number

--- string_calculator.py
+++ string_calculator.py
@@ -1,7 +1,13 @@
+import re
 class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        
+        negatives = [int(n) for n in re.findall(r'-\d+', numbers)]
+        if negatives:
+            raise ValueError(f"Negative numbers not allowed: {', '.join(map(str, negatives))}")
+        
         if numbers.startswith("//"):
             delimiter, numbers = numbers[2:].split("\n", 1)
             numbers = numbers.replace(delimiter, ",")
