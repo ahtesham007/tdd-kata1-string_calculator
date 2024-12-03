b59df42e2cfb232ca31d23417ded36bb4c3adc60
ahtesham@007 <zsyedahtesham@yahoo.com>
2024-12-03 20:00:34 +0530

    added code for delimeter

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,9 @@ class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        if numbers.startswith("//"):
+            delimiter, numbers = numbers[2:].split("\n", 1)
+            numbers = numbers.replace(delimiter, ",")
         numbers = numbers.replace("\n", ",")
+
         return int(sum(map(int, numbers.split(',')))) 
