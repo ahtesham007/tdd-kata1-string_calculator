a0c2eff09df09f8d7e26136a1173540ea680d884
ahtesham@007 <zsyedahtesham@yahoo.com>
2024-12-03 21:33:15 +0530

    added code for multiple delimeter short length

--- string_calculator.py
+++ string_calculator.py
@@ -10,8 +10,20 @@ class StringCalculator:
         
         if numbers.startswith("//"):
             delimiter, numbers = numbers[2:].split("\n", 1)
-            if '[' in delimiter:
-                delimiter = delimiter[1:-1]
+
+            if '[' in delimiter and ']' in delimiter:
+                t_delimeter = ""
+                delimiters = []
+                for char in delimiter:
+                    if char not in ['[', ']']:
+                        t_delimeter += char
+                    elif char == "]":
+                        delimiters.append(t_delimeter)
+                        t_delimeter = ""
+                        
+                for _ in delimiters:
+                    numbers = numbers.replace(_, ",")
+
             numbers = numbers.replace(delimiter, ",")
         numbers = numbers.replace("\n", ",")
 
