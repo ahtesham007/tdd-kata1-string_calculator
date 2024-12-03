f41c3f9522313776cd6072aa082212a51f521098
ahtesham@007 <zsyedahtesham@yahoo.com>
2024-12-03 21:40:48 +0530

    added code for multiple delimeter long length with optimization

--- string_calculator.py
+++ string_calculator.py
@@ -1,30 +1,27 @@
 import re
+
 class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
         
-        negatives = [int(n) for n in re.findall(r'-\d+', numbers)]
-        if negatives:
-            raise ValueError(f"Negative numbers not allowed: {', '.join(map(str, negatives))}")
-        
         if numbers.startswith("//"):
-            delimiter, numbers = numbers[2:].split("\n", 1)
+            delimiter_part, numbers = numbers[2:].split("\n", 1)
 
-            if '[' in delimiter and ']' in delimiter:
-                t_delimeter = ""
-                delimiters = []
-                for char in delimiter:
-                    if char not in ['[', ']']:
-                        t_delimeter += char
-                    elif char == "]":
-                        delimiters.append(t_delimeter)
-                        t_delimeter = ""
-                        
-                for _ in delimiters:
-                    numbers = numbers.replace(_, ",")
+            delimiters = re.findall(r'\[([^\]]+)\]', delimiter_part)
 
-            numbers = numbers.replace(delimiter, ",")
+            if not delimiters:
+                delimiters = [delimiter_part]
+
+            for delimiter in delimiters:
+                numbers = numbers.replace(delimiter, ",")
+
         numbers = numbers.replace("\n", ",")
 
-        return int(sum(num for num in map(int, numbers.split(',')) if num < 1000))
+        num_list = [int(num) for num in numbers.split(",")]
+
+        negatives = [num for num in num_list if num < 0]
+        if negatives:
+            raise ValueError(f"Negative numbers not allowed: {', '.join(map(str, negatives))}")
+
+        return sum(num for num in num_list if num <= 1000)
