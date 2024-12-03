383a6ee151da891f74d31159a53b0edca8fc0e8b
ahtesham@007 <zsyedahtesham@yahoo.com>
2024-12-03 19:50:49 +0530

    added code for new line

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,5 @@ class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        numbers = numbers.replace("\n", ",")
         return int(sum(map(int, numbers.split(',')))) 
